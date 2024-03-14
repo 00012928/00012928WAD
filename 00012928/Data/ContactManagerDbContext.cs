02b00ca08516d4a3667236ae4f4267b46a5561d4
Otabek <Otabek@DESKTOP-EMUANS7>
2024-03-14 14:31:07 +0500

    Add project files.

---
+++ 00012928/Data/ContactManagerDbContext.cs
@@ -0,0 +1,11 @@
+﻿using _00012928.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _00012928.Data
+{
+    public class ContactManagerDbContext : DbContext
+    {
+        public ContactManagerDbContext(DbContextOptions<ContactManagerDbContext> options) : base(options) { }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ 00012928/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace _00012928.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Category { get; set; }
+    }
+}
